d9b9ffdfba73aea0e20df669a1ef8cd89711446a
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2024-12-20 00:14:08 +0100

    add sample code that set PB4 to high

---
+++ main.c
@@ -0,0 +1,16 @@
+// Offset of the IO registers
+#define IO_OFFSET 0x20
+
+// Pins direction register (input vs. output)
+#define DDRB (*(volatile unsigned char *)(IO_OFFSET + 0x17))
+
+// Output pins data register
+#define PORTB (*(volatile unsigned char *)(IO_OFFSET + 0x18))
+
+#define BIT_SET(__REG, __BIT) __REG |= 1 << __BIT
+#define BIT_CLEAR(__REG, __BIT) __REG &= ~(1 << __BIT)
+
+int main() {
+  BIT_SET(DDRB, 1);
+  BIT_SET(PORTB, 4);
+}
