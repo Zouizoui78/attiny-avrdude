84a2295a3c04906ad94c7c7d7873469bff7890a2
Alexandre Zouiten <alexandre.zouiten1@gmail.com>
2024-12-22 18:55:41 +0100

    add defines for registers bits

--- src/main.c
+++ src/main.c
@@ -3,18 +3,18 @@
 #include "reg.h"
 
 ISR(TIM1_OVF_VEC) {
-    bit_toggle(PORTB, 1);
+    bit_toggle(PORTB, PORTB1);
 }
 
 int main(void) {
     // Configure pin as output
-    bit_set(DDRB, 1);
+    bit_set(DDRB, DDB1);
 
     // Set timer 1 prescaler to 16384
     *TCCR1 = 0b1111;
 
     // Enable timer 1 overflow interrupt
-    bit_set(TIMSK, 2);
+    bit_set(TIMSK, TOIE1);
 
     // Enable interrupts
     sei();
--- src/reg.h
+++ src/reg.h
@@ -10,25 +10,55 @@ typedef volatile unsigned char* reg_t;
 #define REG(address) (reg_t)(REG_OFFSET + address)
 
 
-// Status register
-#define SREG REG(0x3F)
+// Power reduction
+#define PRR REG(0x20)
+#define PRTIM1 3
+#define PRTIM0 2
+#define PRUSI 1
+#define PRADC 0
 
 
 // IO //
 
 // Pins direction (input vs. output)
 #define DDRB REG(0x17)
+#define DDB5 5
+#define DDB4 4
+#define DDB3 3
+#define DDB2 2
+#define DDB1 1
+#define DDB0 0
 
 // Output pins data
 #define PORTB REG(0x18)
+#define PORTB5 5
+#define PORTB4 4
+#define PORTB3 3
+#define PORTB2 2
+#define PORTB1 1
+#define PORTB0 0
 
 
 // Interrupts //
 
 // Timer interrupts mask
 #define TIMSK REG(0x39)
+#define OCIE1A 6
+#define OCIE1B 5
+#define OCIE0A 4
+#define OCIE0B 3
+#define TOIE1 2
+#define TOIE0 1
 
 // Timer 1 control
 #define TCCR1 REG(0x30)
+#define CTC1 7
+#define PWM1A 6
+#define COM1A1 5
+#define COM1A0 4
+#define CS13 3
+#define CS12 2
+#define CS11 1
+#define CS10 0
 
 #endif
